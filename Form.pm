25359f05de9de33776a7e46cbedc67cedecb201b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-01-18 19:38:38 +0100

    Prepare for 0.05 version

--- Form.pm
+++ Form.pm
@@ -15,7 +15,7 @@ Readonly::Scalar my $EMPTY_STR => q{};
 Readonly::Scalar my $LINE_SIZE => 79;
 Readonly::Scalar my $SPACE => q{ };
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Constructor.
 sub new {
@@ -431,6 +431,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut

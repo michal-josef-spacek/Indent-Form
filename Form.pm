c31edec69a58f053fc2b9927f9eb0cfe9b78e5ff
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-01-18 17:15:40 +0100

    Add example to doc with explicit indent

--- Form.pm
+++ Form.pm
@@ -327,6 +327,34 @@ __END__
  # Description: File
  # Author.....: skim.cz
 
+=head1 EXAMPLE4
+
+ use strict;
+ use warnings;
+
+ use Encode qw(decode_utf8 encode_utf8);
+ use Indent::Form;
+
+ # Indent object.
+ my $indent = Indent::Form->new;
+
+ # Input data.
+ my $input_ar = [
+         ['Filename', 'foo.bar'],
+         ['Size', '1456kB'],
+         ['Description', 'File'],
+         ['Author', 'skim.cz'],
+ ];
+
+ # Indent.
+ print encode_utf8($indent->indent($input_ar, decode_utf8('|↔| ')))."\n";
+
+ # Output:
+ # |↔|    Filename: foo.bar
+ # |↔|        Size: 1456kB
+ # |↔| Description: File
+ # |↔|      Author: skim.cz
+
 =head1 DEPENDENCIES
 
 L<Class::Utils>,
